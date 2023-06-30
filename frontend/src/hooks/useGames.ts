bdfaa00c90ab2b69319a39bd915d2240d6c1281c
Paw565pl <paw565pl@gmail.com>
2023-06-30 12:32:31 +0200

    simplified time calculations with ms package

--- frontend/src/hooks/useGames.ts
+++ frontend/src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import ApiClient, { FetchResponse } from "../services/ApiClient";
 import { Platform } from "./useParentPlatforms";
@@ -27,7 +28,7 @@ const useGames = (gameQuery: GameQuery) =>
           page_size: 12,
         },
       }),
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
