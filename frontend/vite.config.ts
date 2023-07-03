34e65565d80c6b2e2cbff125b9a1b9e55919da1a
Paw565pl <paw565pl@gmail.com>
2023-07-03 12:34:20 +0200

    added vite plugin for compressing images

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
+  plugins: [react(), viteCompression(), ViteImageOptimizer()],
 });
