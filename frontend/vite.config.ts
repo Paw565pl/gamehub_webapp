789fd483185c055223ff66057ee67a73c131a116
Paw565 <paw565@o2.pl>
2023-07-04 22:58:01 +0200

    deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
