dd6e5c496078b766a017f33f096baa3550256469
Paw565 <paw565@o2.pl>
2023-07-04 10:52:32 +0200

    added redirect to main page after searching game

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -1,17 +1,22 @@
 import { Box, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 
 const SearchInput = () => {
   const setSearchQuery = useGameQueryStore((s) => s.setSearchQuery);
+  const navigate = useNavigate();
   const searchRef = useRef<HTMLInputElement>(null);
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (searchRef.current) setSearchQuery(searchRef.current.value);
+        if (searchRef.current) {
+          setSearchQuery(searchRef.current.value);
+          navigate("/");
+        }
       }}
     >
       <Box paddingX={{ sm: "1.5em", lg: "4.5em" }}>
