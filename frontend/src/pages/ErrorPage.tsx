2451ea8e8cce9ff02768cf1cef112940b7e44baa
Paw565pl <paw565pl@gmail.com>
2023-07-11 20:23:24 +0200

    simplified error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,20 +1,13 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
-  const error = useRouteError();
-
   return (
     <Box padding={"1em"}>
       <NavBar></NavBar>
       <Box padding={"1em 2em"}>
         <Heading>Oops</Heading>
-        <Text>
-          {isRouteErrorResponse(error)
-            ? "This page does not exist."
-            : "Something went wrong. Please try again."}
-        </Text>
+        <Text>Something went wrong. Please try again</Text>
       </Box>
     </Box>
   );
